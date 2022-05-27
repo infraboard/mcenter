c975e223d13aa2756164d72808abf99f9637a45b
yumaojun03 <18108053819@163.com>
2022-05-27 18:46:17 +0800

    添加resolver

--- client/resolver/resolver.go
+++ client/resolver/resolver.go
@@ -1,1 +1,63 @@
 package resolver
+
+import (
+	"google.golang.org/grpc/resolver"
+)
+
+// Following is an example name resolver. It includes a
+// ResolverBuilder(https://godoc.org/google.golang.org/grpc/resolver#Builder)
+// and a Resolver(https://godoc.org/google.golang.org/grpc/resolver#Resolver).
+//
+// A ResolverBuilder is registered for a scheme (in this example, "example" is
+// the scheme). When a ClientConn is created for this scheme, the
+// ResolverBuilder will be picked to build a Resolver. Note that a new Resolver
+// is built for each ClientConn. The Resolver will watch the updates for the
+// target, and send updates to the ClientConn.
+
+// exampleResolverBuilder is a
+// ResolverBuilder(https://godoc.org/google.golang.org/grpc/resolver#Builder).
+type exampleResolverBuilder struct{}
+
+var (
+	exampleServiceName = "test"
+	exampleScheme      = "mcenter"
+	backendAddr        = "127.0.0.1"
+)
+
+func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &exampleResolver{
+		target: target,
+		cc:     cc,
+		addrsStore: map[string][]string{
+			exampleServiceName: {backendAddr},
+		},
+	}
+	r.start()
+	return r, nil
+}
+func (*exampleResolverBuilder) Scheme() string { return exampleScheme }
+
+// exampleResolver is a
+// Resolver(https://godoc.org/google.golang.org/grpc/resolver#Resolver).
+type exampleResolver struct {
+	target     resolver.Target
+	cc         resolver.ClientConn
+	addrsStore map[string][]string
+}
+
+func (r *exampleResolver) start() {
+	addrStrs := r.addrsStore[r.target.Endpoint]
+	addrs := make([]resolver.Address, len(addrStrs))
+	for i, s := range addrStrs {
+		addrs[i] = resolver.Address{Addr: s}
+	}
+	r.cc.UpdateState(resolver.State{Addresses: addrs})
+}
+func (*exampleResolver) ResolveNow(o resolver.ResolveNowOptions) {}
+func (*exampleResolver) Close()                                  {}
+
+func init() {
+	// Register the example ResolverBuilder. This is usually done in a package's
+	// init() function.
+	resolver.Register(&exampleResolverBuilder{})
+}
