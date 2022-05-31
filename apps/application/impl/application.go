94bca890b63b5dcced1e0f028bcf7b510cbe0aad
yumaojun03 <18108053819@163.com>
2022-05-31 10:00:09 +0800

    update protoc

--- apps/application/impl/application.go
+++ apps/application/impl/application.go
@@ -14,27 +14,27 @@ func (i *impl) ValidateCredential(ctx context.Context, req *application.Validate
 	return nil, status.Errorf(codes.Unimplemented, "method ValidateCredential not implemented")
 }
 
-func (i *impl) CreateService(ctx context.Context, req *application.CreateMicroRequest) (
+func (i *impl) CreateService(ctx context.Context, req *application.CreateApplicationRequest) (
 	*application.Application, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateService not implemented")
 }
 
-func (i *impl) QueryService(ctx context.Context, req *application.QueryMicroRequest) (
+func (i *impl) QueryService(ctx context.Context, req *application.QueryApplicationRequest) (
 	*application.Set, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method QueryService not implemented")
 }
 
-func (i *impl) DescribeService(ctx context.Context, req *application.DescribeMicroRequest) (
+func (i *impl) DescribeService(ctx context.Context, req *application.DeleteApplicationRequest) (
 	*application.Application, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DescribeService not implemented")
 }
 
-func (i *impl) DeleteService(ctx context.Context, req *application.DeleteMicroRequest) (
+func (i *impl) DeleteService(ctx context.Context, req *application.DeleteApplicationRequest) (
 	*application.Application, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteService not implemented")
 }
 
-func (i *impl) RefreshCredential(ctx context.Context, req *application.DescribeMicroRequest) (
+func (i *impl) RefreshCredential(ctx context.Context, req *application.DescribeApplicationRequest) (
 	*application.Application, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RefreshCredential not implemented")
 }
