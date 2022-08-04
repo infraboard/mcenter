3f842992f15b525881bf1a8b64cc349df69f382a
yumaojun03 <18108053819@163.com>
2022-08-04 13:46:19 +0800

    补充scm

---
+++ apps/scm/app.go
@@ -0,0 +1,30 @@
+package scm
+
+import (
+	"fmt"
+	"path"
+)
+
+const (
+	AppName = "scm"
+)
+
+func NewDefaultWebHookEvent() *WebHookEvent {
+	return &WebHookEvent{
+		Commits: []*Commit{},
+	}
+}
+
+func NewProjectSet() *ProjectSet {
+	return &ProjectSet{
+		Items: []*Project{},
+	}
+}
+
+func (e *WebHookEvent) ShortDesc() string {
+	return fmt.Sprintf("%s %s [%s]", e.Ref, e.EventName, e.ObjectKind)
+}
+
+func (e *WebHookEvent) GetBranche() string {
+	return path.Base(e.GetRef())
+}
