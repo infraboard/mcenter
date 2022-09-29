52fbbc6b73bd6ef567caf92edf6b62cbcede4808
yumaojun03 <18108053819@163.com>
2022-09-29 16:52:03 +0800

    添加项目工时管理

--- apps/config/config_enum.pb.go
+++ apps/config/config_enum.pb.go
@@ -53,3 +53,93 @@ func (t *TYPE) UnmarshalJSON(b []byte) error {
 	*t = ins
 	return nil
 }
+
+// ParseFORMATFromString Parse FORMAT from string
+func ParseFORMATFromString(str string) (FORMAT, error) {
+	key := strings.Trim(string(str), `"`)
+	v, ok := FORMAT_value[strings.ToUpper(key)]
+	if !ok {
+		return 0, fmt.Errorf("unknown FORMAT: %s", str)
+	}
+
+	return FORMAT(v), nil
+}
+
+// Equal type compare
+func (t FORMAT) Equal(target FORMAT) bool {
+	return t == target
+}
+
+// IsIn todo
+func (t FORMAT) IsIn(targets ...FORMAT) bool {
+	for _, target := range targets {
+		if t.Equal(target) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// MarshalJSON todo
+func (t FORMAT) MarshalJSON() ([]byte, error) {
+	b := bytes.NewBufferString(`"`)
+	b.WriteString(strings.ToUpper(t.String()))
+	b.WriteString(`"`)
+	return b.Bytes(), nil
+}
+
+// UnmarshalJSON todo
+func (t *FORMAT) UnmarshalJSON(b []byte) error {
+	ins, err := ParseFORMATFromString(string(b))
+	if err != nil {
+		return err
+	}
+	*t = ins
+	return nil
+}
+
+// ParseSTATUSFromString Parse STATUS from string
+func ParseSTATUSFromString(str string) (STATUS, error) {
+	key := strings.Trim(string(str), `"`)
+	v, ok := STATUS_value[strings.ToUpper(key)]
+	if !ok {
+		return 0, fmt.Errorf("unknown STATUS: %s", str)
+	}
+
+	return STATUS(v), nil
+}
+
+// Equal type compare
+func (t STATUS) Equal(target STATUS) bool {
+	return t == target
+}
+
+// IsIn todo
+func (t STATUS) IsIn(targets ...STATUS) bool {
+	for _, target := range targets {
+		if t.Equal(target) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// MarshalJSON todo
+func (t STATUS) MarshalJSON() ([]byte, error) {
+	b := bytes.NewBufferString(`"`)
+	b.WriteString(strings.ToUpper(t.String()))
+	b.WriteString(`"`)
+	return b.Bytes(), nil
+}
+
+// UnmarshalJSON todo
+func (t *STATUS) UnmarshalJSON(b []byte) error {
+	ins, err := ParseSTATUSFromString(string(b))
+	if err != nil {
+		return err
+	}
+	*t = ins
+	return nil
+}
