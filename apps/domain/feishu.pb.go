29900b5b76e25f997b25cc58a8a1c37a9d775408
yumaojun03 <18108053819@163.com>
2022-12-06 11:25:11 +0800

    补充example

--- apps/domain/feishu.pb.go
+++ apps/domain/feishu.pb.go
@@ -26,14 +26,14 @@ type FeishuConfig struct {
 	unknownFields protoimpl.UnknownFields
 
 	// 飞书应用凭证, Oauth2.0时 也叫client_id
-	// @gotags: bson:"app_id" json:"app_id"
-	AppId string `protobuf:"bytes,1,opt,name=app_id,json=appId,proto3" json:"app_id" bson:"app_id"`
+	// @gotags: bson:"app_id" json:"app_id" env:"FEISHU_APP_ID"
+	AppId string `protobuf:"bytes,1,opt,name=app_id,json=appId,proto3" json:"app_id" bson:"app_id" env:"FEISHU_APP_ID"`
 	// 飞书应用凭证, Oauth2.0时 也叫client_secret
-	// @gotags: bson:"app_secret" json:"app_secret"
-	AppSecret string `protobuf:"bytes,2,opt,name=app_secret,json=appSecret,proto3" json:"app_secret" bson:"app_secret"`
+	// @gotags: bson:"app_secret" json:"app_secret" env:"FEISHU_APP_SECRET"
+	AppSecret string `protobuf:"bytes,2,opt,name=app_secret,json=appSecret,proto3" json:"app_secret" bson:"app_secret" env:"FEISHU_APP_SECRET"`
 	// Oauth2.0时, 应用服务地址页面
-	// @gotags: bson:"redirect_uri" json:"redirect_uri"
-	RedirectUri string `protobuf:"bytes,3,opt,name=redirect_uri,json=redirectUri,proto3" json:"redirect_uri" bson:"redirect_uri"`
+	// @gotags: bson:"redirect_uri" json:"redirect_uri" env:"FEISHU_REDIRECT_URI"
+	RedirectUri string `protobuf:"bytes,3,opt,name=redirect_uri,json=redirectUri,proto3" json:"redirect_uri" bson:"redirect_uri" env:"FEISHU_REDIRECT_URI"`
 	// 飞书token
 	// @gotags: bson:"token" json:"token"
 	Token *FeishuAccessToken `protobuf:"bytes,4,opt,name=token,proto3" json:"token" bson:"token"`
--- apps/token/provider/feishu/feishu_test.go
+++ apps/token/provider/feishu/feishu_test.go
@@ -1,1 +1,40 @@
 package feishu_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/infraboard/mcenter/apps/domain"
+	"github.com/infraboard/mcenter/apps/token/provider/feishu"
+	"github.com/infraboard/mcube/logger/zap"
+)
+
+var (
+	client *feishu.Feishu
+)
+
+func TestGetUserInfo(t *testing.T) {
+	u, err := client.GetUserInfo(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(u)
+}
+
+func init() {
+	zap.DevelopmentSetup()
+
+	// 加载测试配置
+	conf := domain.NewDefaultFeishuConfig()
+	if err := env.Parse(conf); err != nil {
+		panic(err)
+	}
+
+	// 登陆
+	client = feishu.NewFeishuClient(conf)
+	err := client.Login(ctx, os.Getenv("AUTH_CODE"))
+	if err != nil {
+		panic(err)
+	}
+}
